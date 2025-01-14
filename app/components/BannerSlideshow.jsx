65409954f2270cfb39fdfc4ee188b8899b028b98
Jad Assaf <jad.a.assaf@gmail.com>
2025-01-14 20:53:36 +0200

    Banners

--- app/components/BannerSlideshow.jsx
+++ app/components/BannerSlideshow.jsx
@@ -2,9 +2,9 @@ import React, {useState, useEffect} from 'react';
 
 const BannerSlideshow = () => {
   const images = [
-    'https://cdn.shopify.com/s/files/1/0887/6018/9225/files/lotus-design-n-print-wRzBarqn3hs-unsplash_1.jpg?v=1723744815',
-    'https://cdn.shopify.com/s/files/1/0887/6018/9225/files/aaron-huber-G7sE2S4Lab4-unsplash.jpg?v=1723744895',
-    'https://cdn.shopify.com/s/files/1/0887/6018/9225/files/francesca-tosolini-tHkJAMcO3QE-unsplash_2.jpg?v=1723745417',
+    'https://cdn.shopify.com/s/files/1/0887/6018/9225/files/lotus-design-n-print-wRzBarqn3hs-unsplash_1.jpg?v=1723744815&quality=10',
+    'https://cdn.shopify.com/s/files/1/0887/6018/9225/files/aaron-huber-G7sE2S4Lab4-unsplash.jpg?v=1723744895&quality=10',
+    'https://cdn.shopify.com/s/files/1/0887/6018/9225/files/francesca-tosolini-tHkJAMcO3QE-unsplash_2.jpg?v=1723745417&quality=10',
     // Add more image URLs here
   ];
 
@@ -19,11 +19,11 @@ const BannerSlideshow = () => {
   }, [images.length]);
 
   return (
-    <div style={{width: '100vw', height: '50vh', overflow: 'hidden'}}>
+    <div style={{width: '100vw', height: 'auto', overflow: 'hidden'}}>
       <img
         src={images[currentIndex]}
         alt={`Slide ${currentIndex}`}
-        style={{width: '100%', height: '100%', objectFit: 'cover'}}
+        style={{width: '100%', height: 'auto', objectFit: 'cover'}}
       />
     </div>
   );
